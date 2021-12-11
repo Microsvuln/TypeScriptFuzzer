64fea932cbbc86acca4eab4e8c97dc66e01a7039
“4testaccount” <yang.mingke@foxmail.com>
2021-12-11 23:11:09 +0800

    add test file

---
+++ dist/a.js
@@ -0,0 +1,17 @@
+"use strict";
+let aa = 1;
+let ab = "2";
+let ac = [3, 4];
+let ad = false;
+let ae = 1;
+let af = ac[aa];
+// var sourceCode = `
+// let cc:number[] = [7, 8, 9];
+// let ccc: number = cc[0];
+// let cccc: number = 1;
+// let ccccc: string = "1";
+// `.trim();
+// console.log("before mutate: " + sourceCode);
+// const project = new Project();
+// project.createSourceFile("/test.ts", sourceCode);
+//# sourceMappingURL=a.js.map
